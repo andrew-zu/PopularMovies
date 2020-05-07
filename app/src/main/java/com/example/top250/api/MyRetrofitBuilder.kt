bdd648bbb0ac34c33960443c4e3ea19e38906cf7
Andrii Zubenko <andrei.zubenko@gmail.com>
2020-05-07 16:02:51 -0700

    logger for okHTTP

--- app/src/main/java/com/example/top250/api/MyRetrofitBuilder.kt
+++ app/src/main/java/com/example/top250/api/MyRetrofitBuilder.kt
@@ -1,14 +1,20 @@
 package com.example.top250.api
 
 import com.example.top250.utils.BASE_URL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object MyRetrofitBuilder{
+    val logging = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    val httpClient = OkHttpClient.Builder().addInterceptor(logging)
+
 
     val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
+        .client(httpClient.build())
         .build()
 
     val apiService = retrofit.create(ApiService::class.java)
